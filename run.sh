09802bf4d04d23896401950cbbd0e9b2bf591f78
babfrag <babfrag@gmail.com>
2016-07-27 01:14:54 +0200

    delete directory for restartability

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 # Docker script to configure and start an PokemonGoBot
 #
 
+# Remove if exists (restartability)
+rm -rf /PokemonGoBot
+
 # see https://github.com/jabbink/PokemonGoBot/blob/develop/README.md
 git clone https://github.com/jabbink/PokemonGoBot.git && cd PokemonGoBot
 git submodule update --init --recursive
